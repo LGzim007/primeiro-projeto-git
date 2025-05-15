6a689091c7d4c6a4fad4ebc0717fa05dd3fb47f6
Gonzaga <gonzagalucasadm@gmail.com>
2025-05-15 14:21:09 -0400

    Foram adicionados os arquivos script1.js, script2.js, script3.js e script4.js ao repositório do github

--- js/script1.js
+++ js/script1.js
@@ -4,4 +4,4 @@ let numero = Number(prompt('Digite um número: '));
 let calculo = numero - (1);
 let calculo2 = numero + (1);
 
-alert(' O antecessor de ' + numero + ' é '  + calculo + ' e o sucessor é ' + ca)
+alert(' O antecessor de ' + numero + ' é '  + calculo + ' e o sucessor é ' + calculo2)
